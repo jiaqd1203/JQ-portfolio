8a5cf4cfb3abdce82c6e158a0bd3c0fbb981ea55
jiaqd1203 <jiaqd1203@gmail.com>
2021-02-21 23:13:11 +0000

    draft slideproject

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 window.onload = function(){
-    
+    var slideIn = document.getElementsByClassName('project');
+    function slide(){
+        slideIn.forEach(sliderimage => {
+            // 滑动到图片显示的一半
+            const slideAt = window.innerHeight + window.scrollY - sliderimage.height / 2;
+            // 图片底部距文档顶部的距离
+            const imageBottom = sliderimage.offsetTop + sliderimage.height;
+            // 图片是否已经显示了一半
+            const isHalfShown = slideAt > sliderimage.offsetTop;
+            // 图片是否已经被完全滚动出去
+            const isNotScrolledPast = window.scrollY < imageBottom;
+            if (isHalfShown && isNotScrolledPast) {
+              sliderimage.classList.add('active');
+            } else {
+              sliderimage.classList.remove('active');
+            }
+          });
+
+        }
+    window.addEventListener('scroll', slide);
 }
