637cf97e79f6ecbd78931d2a9741e98370f7f10a
jiaqd1203 <jiaqd1203@gmail.com>
2021-02-22 23:46:03 +0000

    slideIn modify

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 window.onload = function(){
+  // 不加[]代表数组，[]代表具体元素
     var slideIn = document.getElementsByClassName('project');
     function slide(){
-        slideIn.forEach(sliderimage => {
-            // 滑动到图片显示的一半
-            const slideAt = window.innerHeight + window.scrollY - sliderimage.height / 2;
-            // 图片底部距文档顶部的距离
-            const imageBottom = sliderimage.offsetTop + sliderimage.height;
-            // 图片是否已经显示了一半
-            const isHalfShown = slideAt > sliderimage.offsetTop;
-            // 图片是否已经被完全滚动出去
-            const isNotScrolledPast = window.scrollY < imageBottom;
-            if (isHalfShown && isNotScrolledPast) {
-              sliderimage.classList.add('active');
-            } else {
-              sliderimage.classList.remove('active');
-            }
-          });
-
-        }
+      for(var i = 0; i<slideIn.length; i++){
+        // 滚动到project框从底部探出一半的位置(视口viewport高度+页面滚动的距离而不是滚动条的-图片高度一半)
+        var slideAt = window.innerHeight + window.scrollY - slideIn.height / 2;
+        // 图片底部距文档顶部的距离（整个页面的顶部而不是当前视口的顶部+project的高度）
+        var imageBottom = sliderIn.offsetTop + sliderIn.height;
+        // 图片是否已经从底部出现并且显示了一半
+        var halfProjectShown = slideAt > sliderIn.offsetTop;
+        // 图片是否已经被完全滚动到从页面消失（总滚动距离和图片底部到顶部距离）
+        var projectGone = window.scrollY < imageBottom;       
+        if ( halfProjectShown && projectGone) {
+          // 给project加个active类
+          sliderIn.classList.add('active');
+      } else {
+          //移除active类 
+          sliderIn.classList.remove('active');
+      }
+      }
+    }
     window.addEventListener('scroll', slide);
 }
